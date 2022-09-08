f1b1957ee13723da8384cd984ca1269456e2579e
Life <Life@DESKTOP-PKIILDE>
2022-09-08 22:05:12 +0300

    Add some code for ArrayList exercise

--- src/com/astamatii/javarushexc/L2_ArrayListUses.java
+++ src/com/astamatii/javarushexc/L2_ArrayListUses.java
@@ -1,6 +1,8 @@
 package com.astamatii.javarushexc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class L2_ArrayListUses {
 
@@ -26,6 +28,28 @@ public class L2_ArrayListUses {
 		for(Integer i : arrayList) {
 			System.out.println(i);
 		}
+		
+		System.out.println("__________________");
+		
+		int [][][] arr1 = new int[][][] {{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}}};
+		int [][][] arr2 = new int[][][] {{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}}};
+		
+		Integer [] arr3 = new Integer[] {1,2,3};
+		Integer [] arr4 = new Integer[] {1,2,4};
+		
+		System.out.println(Arrays.equals(arr3, arr4));
+		System.out.println(Arrays.deepEquals(arr3, arr4));
+		
+		System.out.println(Arrays.equals(arr1, arr2));
+		System.out.println(Arrays.deepEquals(arr1, arr2));
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Collections.addAll(list, 11, 2, 23, 5, 15, 4, 2, 4);
+
+		//Collections.sort(list);  // 2, 2, 4, 4, 5, 11, 15, 23
+
+		int index = Collections.binarySearch(list, 11);    
+		System.out.println(index);
 		
 
 	}
