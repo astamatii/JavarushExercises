3fab8b0412084516d14f65abd635e15fa6c07a7c
Alexandr Stamatii <stam.sasha@gmail.com>
2022-09-09 18:11:13 +0300

    Some code with Collections

--- src/com/astamatii/javarushexc/Main.java
+++ src/com/astamatii/javarushexc/Main.java
@@ -10,26 +10,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int [][][] arr1 = new int[][][] {{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}}};
-		int [][][] arr2 = new int[][][] {{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}},{{1,1},{1,1},{1,1}}};
-		
-		Integer [] arr3 = new Integer[] {1,2,3};
-		Integer [] arr4 = new Integer[] {1,2,4};
-		
-		System.out.println(Arrays.equals(arr3, arr4));
-		System.out.println(Arrays.deepEquals(arr3, arr4));
-		
-		System.out.println(Arrays.equals(arr1, arr2));
-		System.out.println(Arrays.deepEquals(arr1, arr2));
-		
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		Collections.addAll(list, 11, 2, 23, 5, 15, 4, 2, 4);
-
-		//Collections.sort(list);  // 2, 2, 4, 4, 5, 11, 15, 23
-
-		int index = Collections.binarySearch(list, 11);    
-		System.out.println(index);
-		
+		String[] names = {"John","Sara","Jack"};
+		for (int i = 0; i < names.length; ++i) {
+			System.out.println(names[i]);
+		}
+		System.out.println();
+		for (int i = 0; i < names.length;) {
+			System.out.println(names[i++]);
+		}
 	}
 
 }
