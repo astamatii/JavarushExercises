ac2bf010619e2ce3e6e7c22b140b4392d020cebf
Life <Life@DESKTOP-PKIILDE>
2022-09-12 01:54:38 +0300

    Add some code for switch()

--- src/com/astamatii/javarushexc/L4_9_Switch.java
+++ src/com/astamatii/javarushexc/L4_9_Switch.java
@@ -1,8 +1,23 @@
 package com.astamatii.javarushexc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class L4_9_Switch {
 
+	public static ArrayList<String> planets = new ArrayList<>();
+
 	public static void main(String[] args) {
+		Collections.addAll(planets, "Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus",
+				"Neptune");
+		int planet = 8;
+		System.out.println(planets.get(planet) + " atmosphere is made of:");
+		for (String atm : getPlanetAtmosphere(planet)) {
+			System.out.println(atm);
+		}
+		System.out.println();
 		System.out.println(getMonthByIndex(4));
 		System.out.println(getMonthByIndex(8));
 		System.out.println(getMonthByIndex(12));
@@ -13,10 +28,66 @@ public class L4_9_Switch {
 		System.out.println();
 		System.out.println(getShapeNameByCountOfCorners(3));
 		System.out.println(getShapeNameByCountOfCorners(5));
-		System.out.println(getShapeNameByCountOfCorners(1));
+		System.out.println(getShapeNameByCountOfCorners(10));
+		System.out.println();
+		System.out.println(getValue(1));
+		
 	}
 
-	public static String getShapeNameByCountOfCorners(int countOfCorner) { //switch as expression
+	
+	public static int getValue(int count) {
+		
+		// On Java 13 version:
+		int value = switch (count) {
+		  case 1:
+		     yield 12;
+		  case 2:
+		     yield 32;
+		  case 3:
+		     yield 52;
+		  default:
+		     yield 0;
+		};
+		
+		// On Java 12 version:
+//		int value = switch (count) {
+//		  case 1:
+//		     break 12;
+//		  case 2:
+//		     break 32;
+//		  case 3:
+//		     break 52;
+//		  default:
+//		     break 0;
+//		};
+		
+		return value;
+	}
+	
+	// Not recommended version, just for showing 
+	public static List<String> getPlanetAtmosphere(int seqNumberFromSun) {
+		List<String> result = new ArrayList<>();
+		switch (seqNumberFromSun) {
+		case 1:
+			result.add("Нет атмосферы");
+			break;
+		case 3:
+			result.add("Азот");
+			result.add("Кислород");
+		case 2:
+		case 4:
+			result.add("Углекислый газ");
+			break;
+		case 7, 8:
+			result.add("Метан");
+		case 5, 6:
+			result.add("Водород");
+			result.add("Гелий");
+		}
+		return result;
+	}
+
+	public static String getShapeNameByCountOfCorners(int countOfCorner) { // switch as expression
 		return switch (countOfCorner) {
 		case 3 -> "Треугольник";
 		case 4 -> "Четырехугольник";
@@ -24,8 +95,9 @@ public class L4_9_Switch {
 		case 6 -> "Шестиугольник";
 		case 7 -> "Семиугольник";
 		case 8 -> "Восьмиугольник";
+		case 9, 10 -> "Многоугольник";
 		default -> "Другая фигура";
-		};		
+		};
 	}
 
 	public static String getTranslationForDayOfWeek(String ru) {
