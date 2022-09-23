f27077f94eb37becb30762167c7ac739294e2c4b
alexandrstamatii <stam.sasha@gmail.com>
2022-09-23 17:05:16 +0300

    Adding Try-With-Resources Exercises

---
+++ src/com/astamatii/javarushexc/L6_1_TryWithResources.java
@@ -0,0 +1,47 @@
+package com.astamatii.javarushexc;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class L6_1_TryWithResources {
+
+	public static final String PATH = "C:\\Users\\Public\\Documents\\test.txt";
+	
+	public static void main(String[] args) {
+		
+		tryCatchFinally(PATH, 1);
+		tryWithResources(PATH, 2);
+		 
+	}
+
+	public static void tryCatchFinally(String path, int number) {
+		FileOutputStream output = null;
+
+		try {
+			output = new FileOutputStream(path);
+			output.write(number);
+		} catch (IOException e) {
+			System.out.println("tryCatchFinally() Exception: " + e);
+		} finally {
+			try {
+				output.close();
+				System.out.println("Try-Catch-Finally. (Finally Bloc)");
+			} catch (NullPointerException | IOException e) {
+				System.out.println("tryCatchFinally() Exception: " + e);
+				e.printStackTrace();
+			}							
+		}
+	}
+	
+	public static void tryWithResources(String path, int number) {
+		try(FileOutputStream output = new FileOutputStream(path))
+		{
+		   output.write(number);
+		} catch (IOException e) {
+			System.out.println("tryWithResources() Exception: " + e);
+		} finally {
+			System.out.println("Try-With-Resources. (Finally Bloc)");
+		}
+	}
+
+}
