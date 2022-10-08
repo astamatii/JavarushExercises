6ae88aa4da10e1bcbf9cb399bb3b3755c024cd83
alexandrstamatii <stam.sasha@gmail.com>
2022-10-08 12:17:51 +0300

    Added java.time.LocalDate and java.time.LocalTime exercises

---
+++ src/com/astamatii/javarushexc/javasyntaxzero/L7_4_LocalDate_BugFixing.java
@@ -0,0 +1,19 @@
+package com.astamatii.javarushexc.javasyntaxzero;
+
+import java.time.LocalDate;
+
+//В 2010 году человеку исполнилось 40 лет. Мы попытались вычислить год, когда ему исполнилось 30 лет, 
+// и программа выдала 2020 год. Как такое возможно?
+//
+//Измени один символ в коде, чтобы программа вывела 2000 год.
+
+public class L7_4_LocalDate_BugFixing {
+  
+	public static void main(String[] args) {
+        LocalDate jubilee40Years = LocalDate.of(2010, 3, 13);
+
+        LocalDate jubilee30Years = jubilee40Years.minusYears(10);
+
+        System.out.println(Math.abs(jubilee30Years.getYear()));
+    }
+}
