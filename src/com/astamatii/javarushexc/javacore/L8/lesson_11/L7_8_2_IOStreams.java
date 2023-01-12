336963e2eebf5c5c92da6688624963dfc130b96b
alexandrstamatii <stam.sasha@gmail.com>
2023-01-12 16:52:05 +0200

    Solved a level 18 exercise

--- src/com/astamatii/javarushexc/javacore/L8/lesson_11/L7_8_2_IOStreams.java
+++ src/com/astamatii/javarushexc/javacore/L8/lesson_11/L7_8_2_IOStreams.java
@@ -1,12 +1,16 @@
 package com.astamatii.javarushexc.javacore.L8.lesson_11;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Iterator;
 
 //CRUD для таблицы внутри файла.
 //Напиши программу, которая считывает с консоли путь к файлу для операций CRUD 
@@ -34,51 +38,84 @@ import java.util.Arrays;
 //19847983Куртка для сноубордистов, разм10173.991234
 
 public class L7_8_2_IOStreams {
-	public static void main(String[] args) throws Exception {
-//    	String productName = args[1];
-//    	String price = args[2];
-//    	String quantity = args[3];
-		StringBuilder id = new StringBuilder(3);
-//    	
-//    	String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();   
-//    	FileWriter fileWriter = new FileWriter(fileName);
-//    	BufferedReader fileReader = new BufferedReader (new FileReader(fileName));    	
 
-		List<StringBuilder> record = new ArrayList<>();
-		String lastLine, line;
+	// Modify padding and length using StringBuilder:
+	public static void stringBuilder(StringBuilder sb, int n) {
 		StringBuilder str = new StringBuilder();
+		for (int i = 0; i < sb.length(); i++) {
+			str.append(" ");
+		}
 
-		lastLine = "19846   Шорты пляжные синие           159.00  12  ";
-		if (lastLine != null) {
-			id.append(Integer.parseInt(lastLine.split(" ")[0]) + 1);
-			id.append("        ".substring(id.length())).setLength(8);
-		} else
-			id.append(0);
+		sb.append(str.substring(sb.length())).setLength(n);
+	}
 
-		System.out.println(id);// + " ".substring(id.length()));
+	// Modify padding and length using String:
+	public static String strFormat(String str, int n) {
+		return String.format("%-" + n + "s", str).substring(0, n);
+	}
 
-		System.out.println(Arrays.stream(id.toString().split("")).filter(x -> x.equals(" ")).count());
-
-		System.out.println(String.format("%1$" + 8 + "s", 123456789));
+	public static void main(String[] args) throws Exception {
+		String productName;
+		String price;
+		String quantity;
+		String id;		
+		List<String> records;
 		
-//    	switch (args[0]) {
-//    	case "-c":
-//    		fileWriter.write("");
-//    		lastLine = null;
-//    		while((line = fileReader.readLine()) != null) {
-//        		lastLine = line;
-//        	}
-//    		
-//    		if(lastLine != null) {
-//    			id = Integer.parseInt(lastLine.split(" ")[0]) + 1;
-//    		} else id = 0;
-//    		
-//    		
-//    		
-//    		
-//    		
-//    		break;
-//    	}
+		BufferedReader reader;
+		BufferedWriter writer;
+		
+		String args0 = null;
+				
+		try {
+			args0 = args[0];
+		} catch (Exception e) {
+		}
+		
+		switch (args0) {
+		case "-c":
+			//Reading the filename from console
+			reader = new BufferedReader(new InputStreamReader(System.in));
+			String fileName = reader.readLine();
+			reader.close();	
+			
+			//Create new file if it`s needed:
+			try {
+				reader = new BufferedReader(new FileReader(fileName));
+			} catch (FileNotFoundException e) {
+				new FileWriter(fileName).close();
+				reader = new BufferedReader(new FileReader(fileName));
+			}
+			
+			//Loading all records from file if it has:
+			records = new ArrayList<>();
+			while (reader.ready()) {
+				records.add(reader.readLine());
+			}
+			reader.close();
+			
+			//Creating new record
+			if (!records.isEmpty()) {
+				id = strFormat(String.valueOf(Integer.parseInt(records.get(records.size() - 1).split(" ")[0]) + 1), 8);
+			} else
+				id = strFormat("1", 8);
+
+			productName = strFormat(args[1], 30);
+			price = strFormat(args[2], 8);
+			quantity = strFormat(args[3], 4);
+		
+			records.add(id + productName + price + quantity);
+			
+			//Writing all records with new one, into the file:
+			writer = new BufferedWriter(new FileWriter(fileName));
+			for(String record : records)
+				writer.write("\n" + record);
+			
+			writer.close();
+			break;
+
+		default:
+			break;
+		}
 
 	}
 }
