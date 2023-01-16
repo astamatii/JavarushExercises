ac65ddc999fb40954940379982af979fc9230ad7
alexandrstamatii <stam.sasha@gmail.com>
2023-01-16 23:23:10 +0200

    some L18 exercises

--- src/com/astamatii/javarushexc/javacore/L8/lesson_11/L7_8_4_IOStreams.java
+++ src/com/astamatii/javarushexc/javacore/L8/lesson_11/L7_8_4_IOStreams.java
@@ -1,8 +1,11 @@
 package com.astamatii.javarushexc.javacore.L8.lesson_11;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.CharBuffer;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,5 +16,36 @@ import java.util.Set;
 
 public class L7_8_4_IOStreams {
 	public static void main(String[] args) {
+		String args0;
+		try {
+			args0 = args[0];
+			FileReader fileReader;
+			Set<Character> letters = new HashSet<>();
+			for (char i = 'a'; i <= 'z'; i++) {
+				letters.add(i);
+			}
+			int count = 0;
+			
+			char[] buffer = new char[3];
+			
+			try {
+				fileReader = new FileReader(args0);
+				while(fileReader.ready()) {
+					fileReader.read(buffer);
+					for(Character c : letters) {
+						for( int i = 0; i < buffer.length; i++)
+							if(c.charValue() == buffer[i]) count++;
+					}
+				}
+				fileReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(count);
+		} catch (Exception e) {
+		}
+		
+		
     }
 }
